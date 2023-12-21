94e255ace86e43b9d46d306533fba38ff36f2435
3Kxr <1363013478@qq.com>
2023-12-21 21:18:39 +0800

    update interface

--- js/dynamicJson.js
+++ js/dynamicJson.js
@@ -1,65 +1,12 @@
-let dynamicJson = [
-  {
-    class: "content_card",
-    path: "./img/news/25wan.jpg",
-    title: "女子炒币“赚大钱”，一个月暴“负”25万！",
-    author: "国家反诈中心",
-    time: "2023-12-15",
-  },
-  {
-    class: "content_card",
-    path: "./img/news/img3.jpg",
-    title: "警惕!“接到百万保障扣费”电话，就是诈骗！",
-    author: "国家反诈中心",
-    time: "2023-11-30",
-  },
-  {
-    class: "content_card",
-    path: "./img/news/img3.jpg",
-    title: "投资理财低风险、高回报？全是套路！",
-    author: "国家反诈中心",
-    time: "2023-11-29",
-  },
-  {
-    class: "content_card",
-    path: "./img/news/img3.jpg",
-    title: "买卖“两卡”？可“刑”可“拷”！",
-    author: "国家反诈中心",
-    time: "2023-11-27",
-  },
-  {
-    class: "content_card",
-    path: "./img/news/img3.jpg",
-    title: "“乌兰牧骑+反诈宣传”包头公安解锁反诈新流量密码",
-    author: "国家反诈中心",
-    time: "2023-11-15",
-  },
-  {
-    class: "content_card",
-    path: "./img/news/img3.jpg",
-    title:
-      "贵州省2023年“全民反诈乡村行”——流动乡村小舞台走进白云，唱响美丽乡村反诈之行",
-    author: "国家反诈中心",
-    time: "2023-10-29",
-  },
-  {
-    class: "content_card",
-    path: "./img/news/img3.jpg",
-    title: "“百万保障”诈骗新手段？预警劝阻带你识破骗子的套路",
-    author: "国家反诈中心",
-    time: "2023-11-27",
-  },
-  {
-    class: "content_card",
-    path: "./img/news/facetime.jpg",
-    title: "FaceTime来电，接还是不接？",
-    author: "国家反诈中心",
-    time: "2023-11-22",
-  },
-];
+let url = "http://rap2api.taobao.org/app/mock/316321/djc";
 var dynamic = document.getElementById("dynamic_main");
 
-for (var v of dynamicJson) {
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    // 在这里处理接口返回的数据
+    // console.log(data);
+     for (var v of data) {
   /* 创建一个新的div元素 */
   var newDiv = document.createElement("div");
   newDiv.className = v.class;
@@ -75,4 +22,9 @@ for (var v of dynamicJson) {
     `;
   /* 将新创建的div添加到容器中 */
   dynamic.appendChild(newDiv);
-}
+     }
+  })
+  .catch((error) => {
+    // 在这里处理错误
+    console.error("Error:", error);
+  });

6a7e60395b690e7aca961eae0cd291b443e2af40
3Kxr <1363013478@qq.com>
2023-12-25 15:44:45 +0800

    f1x bug

--- js/dynamicJson.js
+++ js/dynamicJson.js
@@ -1,22 +1,22 @@
 let url = "http://rap2api.taobao.org/app/mock/316321/djc/dynamicjson";
 var dynamic = document.getElementById("dynamic_main");
 
-fetch(url, {
-  method: "GET",
-  headers: {
-    "Content-Type": "application/json",
-  },
-})
-  .then((response) => response.json())
-  .then((data) => {
-    // 在这里处理接口返回的数据
-    // console.log(data);
-    for (var v of data) {
-      /* 创建一个新的div元素 */
-      var newDiv = document.createElement("div");
-      newDiv.className = v.class;
+ fetch(url, {
+   method: "GET",
+   headers: {
+     "Content-Type": "application/json",
+   },
+ })
+   .then((response) => response.json())
+   .then((data) => {
+     // 在这里处理接口返回的数据
+     // console.log(data);
+     for (var v of data) {
+       /* 创建一个新的div元素 */
+       var newDiv = document.createElement("div");
+       newDiv.className = v.class;
 
-      newDiv.innerHTML = `
+       newDiv.innerHTML = `
     
           <div class="text">
             <p>${v.title}</p>
@@ -25,11 +25,11 @@ fetch(url, {
           <img src="${v.path}" alt="">
         
     `;
-      /* 将新创建的div添加到容器中 */
-      dynamic.appendChild(newDiv);
-    }
-  })
-  .catch((error) => {
-    // 在这里处理错误
-    console.error("Error:", error);
-  });
+       /* 将新创建的div添加到容器中 */
+       dynamic.appendChild(newDiv);
+     }
+   })
+   .catch((error) => {
+     // 在这里处理错误
+     console.error("Error:", error);
+   });
--- js/knowledgeJson.js
+++ js/knowledgeJson.js
@@ -1,22 +1,22 @@
 let json_url = "http://rap2api.taobao.org/app/mock/316321/djc/knowledgejson";
 var dynamic_id = document.getElementById("knowledge_main");
 
-fetch(json_url, {
+ fetch(json_url, {
    method: "GET",
    headers: {
      "Content-Type": "application/json",
    },
  })
-  .then((response) => response.json())
-  .then((data_json) => {
-    // 在这里处理接口返回的数据
-    // console.log(data);
-    for (var v of data_json) {
-      /* 创建一个新的div元素 */
-      var newDiv = document.createElement("div");
-      newDiv.className = v.class;
+   .then((response) => response.json())
+   .then((data_json) => {
+     // 在这里处理接口返回的数据
+     // console.log(data);
+     for (var v of data_json) {
+       /* 创建一个新的div元素 */
+       var newDiv = document.createElement("div");
+       newDiv.className = v.class;
 
-      newDiv.innerHTML = `
+       newDiv.innerHTML = `
         
           <div class="text">
             <p>${v.title}</p>
@@ -25,12 +25,12 @@ fetch(json_url, {
           <img src="${v.path}" alt="">
         
     `;
-      /* 将新创建的div添加到容器中 */
-      dynamic_id.appendChild(newDiv);
-    }
-  })
-  
-  .catch((error) => {
-    // 在这里处理错误
-    console.error("Error:", error);
-  });
+       /* 将新创建的div添加到容器中 */
+       dynamic_id.appendChild(newDiv);
+     }
+   })
+
+   .catch((error) => {
+     // 在这里处理错误
+     console.error("Error:", error);
+   });
